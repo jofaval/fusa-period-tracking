58f058cf8bb38495b85b9e3e57663756c522021c
pepe_fabra_valverde <jofaval@outlook.com>
2022-07-21 23:57:19 +0200

    #7 Create Gender entity

---
+++ app/src/entities/gender/gender.entity.ts
@@ -0,0 +1,25 @@
+// Entities
+import Entity from '../entity';
+// Helpers
+import { serialize } from '../../architecture/helpers/entity.helpers';
+
+export default class Gender extends Entity implements IGenderEntity {
+  public pronouns: IPronounsEntity;
+
+  constructor(name: string, pronouns: IPronounsEntity) {
+    super(name);
+    this.pronouns = pronouns;
+  }
+
+  serialize(): string {
+    return serialize({
+      name: this.name,
+      pronouns: this.pronouns.serialize(),
+    });
+  }
+
+  toString(): string {
+    const { name, pronouns } = this;
+    return `Gender "${name}" uses the pronouns: ${pronouns.serialize()}`;
+  }
+}
