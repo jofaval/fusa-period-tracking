a54378995c37d2877039e2f04e50b5ca017c5ab1
pepe_fabra_valverde <jofaval@outlook.com>
2022-07-21 23:59:24 +0200

    #8 Export default entities

---
+++ app/src/entities/index.ts
@@ -0,0 +1,5 @@
+import Gender from './gender/gender.entity';
+import Profile from './profile/profile.entity';
+import Pronouns from './pronouns/pronouns.entity';
+
+export { Gender, Profile, Pronouns };
