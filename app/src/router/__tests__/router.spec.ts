d47d076641c9160be657e5b147b74e448ee1ea28
pepe_fabra_valverde <jofaval@outlook.com>
2022-07-23 23:27:15 +0200

    #9 Implement base routing system

---
+++ app/src/router/__tests__/router.spec.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest';
+
+describe('Router', () => {
+  it('should something', () => {
+    expect(true).toBeTruthy();
+  });
+});
--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -1,18 +1,4 @@
-// Vendors
-import { createRouter, createWebHashHistory } from 'vue-router';
-// Constants
-import constants from './constants/routes.constants';
-import { configConstants } from '../constants';
-
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes: constants.ROUTES,
-});
-
-router.afterEach((to, from, failure) => {
-  const toAsString = to.toString();
-  document.title =
-    toAsString in constants.TITLES ? constants.TITLES[toAsString] : configConstants.APP_NAME;
-});
+// Router
+import router from './router';
 
 export default router;
