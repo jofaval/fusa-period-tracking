f310391670f3f24522835d2b441e1cbe9bc3975c
pepe_fabra_valverde <jofaval@outlook.com>
2022-07-24 23:22:53 +0200

    #10 Change title with multilanguage features

--- app/src/langs/en/en.lang.ts
+++ app/src/langs/en/en.lang.ts
@@ -2,4 +2,17 @@ export default {
   home: {
     title: 'Welcome!',
   },
+  pages: {
+    home: {
+      title: 'Home',
+    },
+    fallback: {
+      title: 'Home',
+    },
+    profile: {
+      root: {
+        title: 'Profile',
+      },
+    },
+  },
 };
