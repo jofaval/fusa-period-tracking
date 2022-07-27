897e5e953d615c454e828fdbe7bca49155655012
pepe_fabra_valverde <jofaval@outlook.com>
2022-07-27 12:27:04 +0200

    #30 Implement initial store

---
+++ app/src/store/__tests__/store.spec.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest';
+
+describe('Global store', () => {
+  it('should', () => {
+    expect(true).toBeTruthy();
+  });
+});
---
+++ app/src/store/store.ts
@@ -0,0 +1,47 @@
+// Vendors
+import dbConstants from 'constants/db.constants';
+import themeConstants from 'constants/theme.constants';
+import { defineStore } from 'pinia';
+
+interface IState {
+  db: IDBDatabase | null;
+  dbError: unknown;
+  theme: {
+    current: string;
+  };
+}
+
+export const toggleTheme = (current: string): string =>
+  current === themeConstants.DARK ? themeConstants.LIGHT : themeConstants.DARK;
+
+const store = defineStore('global', {
+  state: (): IState => ({
+    db: null,
+    dbError: null,
+    theme: {
+      current: themeConstants.DEFAULT,
+    },
+  }),
+  getters: {
+    db: (state: IState) => state.db,
+    theme: (state: IState) => state.theme,
+    currentTheme: (state: IState) => state.theme.current,
+  },
+  actions: {
+    initializeIndexedDb() {
+      this.db = null;
+    },
+    initializeStore() {
+      this.initializeIndexedDb();
+      this.setTheme(themeConstants.DEFAULT);
+    },
+    toggleTheme() {
+      this.theme.current = toggleTheme(this.theme.current);
+    },
+    setTheme(newTheme: string) {
+      this.theme.current = newTheme;
+    },
+  },
+});
+
+export default store;
