1431f154cfce4674f2d744f2a2899eeaddb193ba
pepe_fabra_valverde <jofaval@outlook.com>
2022-07-27 16:45:42 +0200

    #100 Husky and imports patch on Github Actions

--- app/src/router/router.ts
+++ app/src/router/router.ts
@@ -4,7 +4,7 @@ import { createRouter, createWebHashHistory, RouteLocationNormalized } from 'vue
 import constants from './constants/routes.constants';
 import { configConstants } from '../constants';
 // Locales
-import i18n from 'langs';
+import i18n from '../langs';
 
 const router = createRouter({
   history: createWebHashHistory(),
