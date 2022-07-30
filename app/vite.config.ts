eb02ae8fcf312bd18f48e0f12681a8b58cdaf578
pepe_fabra_valverde <jofaval@outlook.com>
2022-07-30 02:55:58 +0200

    bugfix: #108 Move logic to vite.config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,17 @@
+// Vendors
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+// Constants
+const configConstants = require('./src/constants/config.constants').default;
+
+const base =
+  // There may be different environments: dev, qa, staging, pre-prod, prod...
+  process.env.NODE_ENV in configConstants.PUBLIC_PATHS
+    ? configConstants.PUBLIC_PATHS[process.env.NODE_ENV]
+    : '/';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base,
 });
