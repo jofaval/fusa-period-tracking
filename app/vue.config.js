b5c77b3e93e3a3542bb66f2c64ecf7207b99de04
pepe_fabra_valverde <jofaval@outlook.com>
2022-07-30 02:55:58 +0200

    bugfix: #108 Move logic to vite.config

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,18 +1,8 @@
 // vue.config.js
-// Constants
-import configConstants from './src/constants/config.constants';
-// Vendors
 const { defineConfig } = require('@vue/cli-service');
-
-const publicPath =
-  // There may be different environments: dev, qa, staging, pre-prod, prod...
-  process.env.NODE_ENV in configConstants.PUBLIC_PATHS
-    ? configConstants.PUBLIC_PATHS[process.env.NODE_ENV]
-    : '/';
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath,
   devServer: {
     disableHostCheck: true,
   },
