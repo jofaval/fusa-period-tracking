f7345eeb646321723c57092c00870e38788b835b
pepe_fabra_valverde <jofaval@outlook.com>
2022-08-08 23:55:35 +0200

    feat(components): (#48, #49, #51) Base components

---
+++ app/src/components/MetricsBirthControlComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    class="metrics__birth-control__container rounded-md bg-orange-500 shadow-lg shadow-orange-500/50 text-white p-3"
+  >
+    <div class="metrics__birth-control__label font-bold">
+      <span class="metrics__birth-control__label__title">{{
+        $t('metrics.birthControl.title')
+      }}</span>
+    </div>
+    <div class="metrics__birth-control">
+      <span class="metrics__birth-control__value"></span>&nbsp;<span
+        class="metrics__birth-control__suffix"
+      ></span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Vendors
+import { defineComponent } from 'vue';
+
+const props = defineProps({
+  value: { type: String, required: true },
+});
+
+defineComponent({
+  name: 'MetricsBirthControlComponent',
+});
+</script>
+
+<style scoped></style>
---
+++ app/src/components/MetricsIntercourseComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="metrics__intercourse__container rounded-md bg-orange-500 shadow-lg shadow-orange-500/50 text-white p-3"
+  >
+    <div class="metrics__intercourse__label font-bold">
+      <span class="metrics__intercourse__label__title">{{ $t('metrics.intercourse.title') }}</span>
+    </div>
+    <div class="metrics__intercourse">
+      <span class="metrics__intercourse__value"></span>&nbsp;<span
+        class="metrics__intercourse__suffix"
+      ></span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Vendors
+import { defineComponent } from 'vue';
+
+const props = defineProps({
+  value: { type: String, required: true },
+});
+
+defineComponent({
+  name: 'MetricsIntercourseComponent',
+});
+</script>
+
+<style scoped></style>
